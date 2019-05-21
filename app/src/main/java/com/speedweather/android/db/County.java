1e8f58311a912a27d8e2dd8835220136e3bafdcc
liulonglonglong <2426536036@qq.com>
2019-05-21 11:19:16 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/speedweather/android/db/County.java
+++ app/src/main/java/com/speedweather/android/db/County.java
@@ -1,8 +1,10 @@
 package com.speedweather.android.db;
+
 import org.litepal.crud.DataSupport;
-public class Country extends DataSupport {
+
+public class County extends DataSupport {
     private int id;
-    private String countryName;
+    private String countyName;
     private String weatherId;
     private int cityId;
     public int getId() {
@@ -11,25 +13,22 @@ public class Country extends DataSupport {
     public void setId(int id) {
         this.id = id;
     }
-    public String getCountryName() {
-        return countryName;
+    public String getCountyName() {
+        return countyName;
     }
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
     }
     public String getWeatherId() {
         return weatherId;
     }
-
     public void setWeatherId(String weatherId) {
         this.weatherId = weatherId;
     }
-
     public int getCityId() {
         return cityId;
     }
-
     public void setCityId(int cityId) {
         this.cityId = cityId;
-    }
-}
+    }}
+
